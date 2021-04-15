2d6d504a19675c4bc50d76d300f2455ad41a1f0d
dariy <dariy@DESKTOP-HL2RNGV>
2021-04-15 11:24:36 +0300

    CalcClass create

---
+++ CalcClass/Calc.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcClass
+{
+    public class Calc
+    {
+        //Використовується глобальна змінна:
+        /// <summary>
+        /// Останнє повідомлення про помилку.
+        /// Поле і властивість для нього
+        /// </summary>
+        private static string _lastError = "";
+        public static string lastError;
+        public double Add(double a, double b)
+        {
+            if (-2147483648 > a + b || 2147483648 < a + b)
+            {
+                _lastError = "Error 06 — Дуже мале, або дуже велике значення числа для int. Числа повинні бути в межах від -2147483648 до 2147483647.";
+                lastError = _lastError;
+                throw new Exception(lastError);
+            }
+            return a + b;
+        }
+             /// <summary>
+             /// функція віднімання чисел а і b
+             /// </summary>
+             /// <param name="a">зменшуване</param>
+             /// <param name="b">від’ємне</param>
+             /// <returns>різниця</returns>
+        public double Sub(double a, double b)
+        {
+            if (-2147483648 > a - b|| 2147483648<a-b)
+            {
+                _lastError = "Error 06 — Дуже мале, або дуже велике значення числа для int. Числа повинні бути в межах від -2147483648 до 2147483647.";
+                lastError = _lastError;
+                throw new Exception(lastError);
+            }
+            return a-b;
+        }
+
+         /// <summary>
+         /// функція множення чисел а і b
+         /// </summary>
+         /// <param name="a">множник</param>
+         /// <param name="b">множник</param> /// <returns>добуток</returns>
+        public double Mult(double a, double b)
+        {
+            if (-2147483648 > a * b || 2147483648 < a * b)
+            {
+                _lastError = "Error 06 — Дуже мале, або дуже велике значення числа для int. Числа повинні бути в межах від -2147483648 до 2147483647.";
+                lastError = _lastError;
+                throw new Exception(lastError);
+            }
+            return a * b;
+        }
+         /// <summary>
+         /// функція знаходження частки
+         /// </summary>
+         /// <param name="a">ділене</param>
+         /// <param name="b">дільник</param>
+         /// <returns>частка</returns>
+        public double Div(double a, double b)
+        {
+            if (b == 0)
+            {
+                _lastError = "Error 09 – Помилка ділення на 0.";
+                lastError = _lastError;
+                throw new Exception(_lastError);
+            }
+            return a / b;
+        }
+
+         /// <summary>
+         /// функція ділення по модулю
+         /// </summary>
+         /// <param name="a">ділене</param>
+         /// <param name="b">дільник</param>
+         /// <returns>остача</returns>
+        public double Mod(double a, double b)
+        {
+
+            return a % b;
+        }
+         /// <summary>
+         /// унарний плюс 
+         /// </summary>
+         /// <param name="a"></param>
+         /// <returns></returns>
+        public double ABS(double a)
+        {
+            return a;
+        }
+         /// <summary>
+         /// унарний мінус 
+         /// </summary>
+         /// <param name="a"></param> /// <returns></returns>
+        public double IABS(double a)
+        {
+            return -a;
+        }
+    }
+}
---
+++ CalcClassTests/CalcTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcClass.Tests
+{
+    [TestClass()]
+    public class CalcTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Calc calc = new Calc();
+            Assert.AreEqual(3, calc.Add(1,2));
+        }
+        [TestMethod()]
+        public void SubTest()
+        {
+            Calc calc = new Calc();
+            Assert.AreEqual(-1, calc.Sub(1, 2));
+        }
+        [TestMethod()]
+        public void MultTest()
+        {
+            Calc calc = new Calc();
+            Assert.AreEqual(4, calc.Mult(2, 2));
+        }
+        [TestMethod()]
+        public void DivTest()
+        {
+            Calc calc = new Calc();
+            Assert.AreEqual(1, calc.Div(2, 2));
+        }
+        [TestMethod()]
+        public void ModTest()
+        {
+            Calc calc = new Calc();
+            Assert.AreEqual(0, calc.Mod(4, 2));
+        }
+        [TestMethod()]
+        public void ABSTest()
+        {
+            Calc calc = new Calc();
+            Assert.AreEqual(5, calc.ABS(5));
+        }
+        [TestMethod()]
+        public void IABSTest()
+        {
+            Calc calc = new Calc();
+            Assert.AreEqual(-5, calc.IABS(5));
+        }
+        [ExpectedException(typeof(Exception), "Error 06 — Дуже мале, або дуже велике значення числа для int. Числа повинні бути в межах від - 2147483648 до 2147483647.")]
+        [TestMethod()]
+        public void AddExcepTest()
+        {
+            Calc calc = new Calc();
+            calc.Add(2147483648, 1);
+        }
+        [ExpectedException(typeof(Exception), "Error 06 — Дуже мале, або дуже велике значення числа для int. Числа повинні бути в межах від - 2147483648 до 2147483647.")]
+        [TestMethod()]
+        public void SubExcepTest()
+        {
+            Calc calc = new Calc();
+            calc.Sub(-2147483648, 1);
+        }
+        [ExpectedException(typeof(Exception), "Error 06 — Дуже мале, або дуже велике значення числа для int. Числа повинні бути в межах від - 2147483648 до 2147483647.")]
+        [TestMethod()]
+        public void MultExcepTest()
+        {
+            Calc calc = new Calc();
+            calc.Mult(214748, 83648);
+        }
+        [ExpectedException(typeof(Exception), "Error 09 – Помилка ділення на 0.")]
+        [TestMethod()]
+        public void DivExcepTest()
+        {
+            Calc calc = new Calc();
+            Assert.AreEqual(1, calc.Div(2, 0));
+        }
+    }
+}
